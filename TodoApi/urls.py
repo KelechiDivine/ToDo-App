7c62ede87ae946ec08b2232c22c9d68a6df84539
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-08-27 16:11:58 +0100

    created a todo serializer

--- TodoApi/urls.py
+++ TodoApi/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from . import views
+from .import views
 
 urlpatterns = [
-    path('', views.getRoutes)
+    path('', views.getRoutes),
+    path('notes/', views.getNotes)
 ]
--- TodoApi/views.py
+++ TodoApi/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import NoteSerializer
+from .models import Note
 
 # Create your views here.
 
@@ -47,3 +49,29 @@ def getRoutes(request):
     ]
     
     return Response(routes)
+
+@api_view(['GET'])
+
+def getNotes(request):
+    note = Note.objects.all()
+    serializer = NoteSerializer(note, many= True)
+    return Response(serializer.data)
+
+
+
+@api_view(['GET'])
+
+def getNote(request, pk):
+    note = Note.objects.get(id= pk)
+    serializer = NoteSerializer(note, many= False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def createNote(request):
+    data = request.data
+    note= Note.objects.create(
+        body= data['body']
+    )
+    serializer = NoteSerializer(note, many= False)
+    
