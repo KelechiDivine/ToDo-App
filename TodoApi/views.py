95e2a8e6ce70741028713ef28c828c0ae2b9a20a
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-08-27 15:15:37 +0100

    installed  django rest_framework

--- TodoApi/views.py
+++ TodoApi/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+@api_view(['GET'])
 
 def getRoutes(request):
     
     routes = [
         {
-            'Endpoint': '/notes',
+            'Endpoint': '/notes/',
             'method': 'GET',
             'body':None,
             'description': 'Return an array of notes'
@@ -42,4 +46,4 @@ def getRoutes(request):
         },
     ]
     
-    return JsonResponse(routes, safe= False)
+    return Response(routes)
--- notes/settings.py
+++ notes/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'TodoApi.apps.TodoapiConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
